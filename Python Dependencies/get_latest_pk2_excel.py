87c29e9d98888c5a4660b556d1110730a7c1cd89
spishyspishem <45132027+spishyspishem@users.noreply.github.com>
2024-02-07 22:40:47 -0600

    Update get_latest_pk2_excel.py

--- Python Dependencies/get_latest_pk2_excel.py
+++ Python Dependencies/get_latest_pk2_excel.py
@@ -1,7 +1,6 @@
 import requests
 
-# url = r'https://raw.githubusercontent.com/spishyspishem/pk2_excel/main/pk2_excel.py'
-url = r'https://raw.githubusercontent.com/spishyspishem/pk2_excel/main/Python%20Dependencies/pk2_excel.py'
+url = r'https://raw.githubusercontent.com/spishyspishem/Js/main/Python%20Dependencies/pk2_excel.py'
 response = requests.get(url, verify=False) # TH has some kind of ssl certificates i can't verify; so disable verification
 print(response.content)
 with open('pk2_excel.py', 'wb') as f:
